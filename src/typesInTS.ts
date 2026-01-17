2e6a161cbf45d47c8a0577c4b3f6148f7491ee5f
addy-301 <vaditya832@gmail.com>
2026-01-17 20:57:13 +0530

    Add initial TypeScript project files including .gitignore, package.json, and tsconfig.json

---
+++ src/typesInTS.ts
@@ -0,0 +1,5 @@
+let drink="coffee"; //infered type
+//type inferencing, understands automatically the type of variable
+
+let drink2:string="tea"; //explicit type
+//type annotation, we explicitly tell the type of variable
---
+++ src/unionAndAny.ts
@@ -0,0 +1,4 @@
+let subs: number | string='1M'; //union type, can hold number or string
+
+let status: 'pending' | 'success' | 'error'='pending'; //literal type, can hold only specific string values
+
