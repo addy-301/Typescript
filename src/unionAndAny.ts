1104fbc9f178d3ab4e2eb3f81f6ec449ffe2892a
addy-301 <vaditya832@gmail.com>
2026-01-17 21:09:20 +0530

    Implement union and literal types in unionAndAny.ts

--- src/unionAndAny.ts
+++ src/unionAndAny.ts
@@ -2,3 +2,17 @@ let subs: number | string='1M'; //union type, can hold number or string
 
 let status: 'pending' | 'success' | 'error'='pending'; //literal type, can hold only specific string values
 
+let seat: 'aisle'|'window'|'middle'='aisle'; //literal type for seat positions
+
+seat='window'; //valid
+
+const orders=['12', '14', '26', '28']
+let currOrder: string | undefined; // Avoid any ]
+//let currOrder; Avoid using any type
+for(let x of orders){
+    if(x==='28'){
+        currOrder=x;
+        break;
+    }
+}
+console.log(currOrder)  
